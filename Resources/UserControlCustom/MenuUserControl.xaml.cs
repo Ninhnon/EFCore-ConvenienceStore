9d87f40c4091d6b6e21be801724f2e39c8458438
Nguyen Trong Ninh <nguyentrongninh24072003@gmail.com>
2023-12-29 19:39:43 +0700

    init project

---
+++ Resources/UserControlCustom/MenuUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ConvenienceStore.Resources.UserControlCustom
+{
+    /// <summary>
+    /// Interaction logic for MenuUserControl.xaml
+    /// </summary>
+    public partial class MenuUserControl : UserControl
+    {
+        public MenuUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Resources/UserControlCustom/TextBoxUC.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ConvenienceStore.Resources.UserControlCustom
+{
+    /// <summary>
+    /// Interaction logic for TextBoxUC.xaml
+    /// </summary>
+    public partial class TextBoxUC : UserControl
+    {
+        public string Hint
+        {
+            get { return (string)GetValue(HintProperty); }
+            set { SetValue(HintProperty, value); }
+        }
+
+        public static readonly DependencyProperty HintProperty = DependencyProperty.Register("Hint", typeof(string), typeof(TextBoxUC));
+
+
+        public string Caption
+        {
+            get { return (string)GetValue(CaptionProperty); }
+            set { SetValue(CaptionProperty, value); }
+        }
+
+        public static readonly DependencyProperty CaptionProperty = DependencyProperty.Register("Caption", typeof(string), typeof(TextBoxUC));
+        public TextBoxUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
